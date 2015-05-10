97e19675798b48d78dd233f469af22c7a02a9988
quernalpanic <quernalpanic@fido.blah>
2015-05-09 22:41:41 -0400

    added script for adding users

---
+++ Addlocalusers andgroups.bat
@@ -0,0 +1,28 @@
+net user ac208n1 student /add
+net user ac208n2 student /add
+net user ac208n3 student /add
+net user ac208n4 student /add
+net user ac208n5 student /add
+net user ac208n6 student /add
+net user ac208n7 student /add
+net user ac208n8 student /add
+net user ac208n9 student /add
+net user ac208n10 student /add
+net user ac208n11 student /add
+net user ac208n12 student /add
+net user ac208n13 student /add
+net user ac208n14 student /add
+net localgroup "Remote Desktop Users" ac208n1 /add
+net localgroup "Remote Desktop Users" ac208n2 /add
+net localgroup "Remote Desktop Users" ac208n3 /add
+net localgroup "Remote Desktop Users" ac208n4 /add
+net localgroup "Remote Desktop Users" ac208n5 /add
+net localgroup "Remote Desktop Users" ac208n6 /add
+net localgroup "Remote Desktop Users" ac208n7 /add
+net localgroup "Remote Desktop Users" ac208n8 /add
+net localgroup "Remote Desktop Users" ac208n9 /add
+net localgroup "Remote Desktop Users" ac208n10 /add
+net localgroup "Remote Desktop Users" ac208n11 /add
+net localgroup "Remote Desktop Users" ac208n12 /add
+net localgroup "Remote Desktop Users" ac208n13 /add
+net localgroup "Remote Desktop Users" ac208n14 /add
